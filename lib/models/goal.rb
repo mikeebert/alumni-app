d76b8b0a86f652a2cc3b114096a0f8ebd8744ac4
Jon Wheeler <jon@doejo.com>
2013-05-08 23:01:13 -0500

    Goal Datastore uses Datastore::Base class

--- lib/models/goal.rb
+++ lib/models/goal.rb
@@ -1,12 +1,10 @@
 class Goal
-
   attr_reader :name, :description, :user_id
   attr_accessor :id
 
   def initialize(params = {})
-    @name = params[:name]
+    @name        = params[:name]
     @description = params[:description]
-    @user_id = params[:user_id]
+    @user_id     = params[:user_id]
   end
-
 end
