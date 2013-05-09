ef707f6fcbd41070adc911a89e2d4efb1bbbd9aa
Jon Wheeler <jon@doejo.com>
2013-05-08 22:59:45 -0500

    Update goal datastroe specs

--- spec/datastore/goal_datastore_spec.rb
+++ spec/datastore/goal_datastore_spec.rb
@@ -1,40 +1,27 @@
-require 'datastore/goal'
-require 'models/goal'
+require 'spec_helper'
 
 describe Datastore::Goal do
+  subject { Datastore::Goal.new }
+  let(:goal1) { Goal.new }
+  let(:goal2) { Goal.new }
 
-  
+  before do
+    subject.save(goal1)
+    subject.save(goal2)
+  end
 
   it "saves a Goal object with an id" do
-    datastore = Datastore::Goal.new
-    goal = Goal.new
-
-    datastore.save(goal)
-
-    datastore.records[1].should == goal
-    goal.id.should == 1
+    subject.records[1].should == goal1
+    expect(goal1.id).to eq 1
+    expect(goal2.id).to eq 2
   end
 
   it "saves two Goals with different ids" do
-    datastore = Datastore::Goal.new
-    goal1 = Goal.new
-    goal2 = Goal.new
-
-    datastore.save(goal1)
-    datastore.save(goal2)
-
-    datastore.records.map { |k, v| k }.uniq.length.should == 2
-    
+    records = subject.records.map { |k, v| k }.uniq.length
+    expect(records).to eq 2
   end
   
   it "returns all users" do
-    datastore = Datastore::Goal.new
-    goal = Goal.new
-    goal2 = Goal.new
-
-    datastore.save(goal)
-    datastore.save(goal2)
-
-    datastore.all.should == [goal, goal2]
+    expect(subject.all).to match_array [goal1, goal2]
   end
 end
