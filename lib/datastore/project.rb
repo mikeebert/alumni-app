3068bfe6d7e975f99c8c93d3c7b995d9b47c0380
Mike Ebert <mike@mikeebert.net>
2013-05-13 21:02:00 -0500

    Makes project inherit from Base

--- lib/datastore/project.rb
+++ lib/datastore/project.rb
@@ -1,21 +1,4 @@
 module Datastore
-  class Project
-    attr_accessor :records
-
-    def initialize
-      @id = 1
-      @records = {}
-    end
-
-    def save(project)
-      project.id = @id
-      @records[@id] = project
-      @id += 1
-    end
-
-    def all
-      @records.map {|k,v| v}
-    end
-
+  class Project < Datastore::Base
   end
 end
